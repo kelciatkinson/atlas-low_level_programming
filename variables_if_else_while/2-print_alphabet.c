8b9397c616d886ecaec1faf577be109ad9fba575
root <kelciatkinson@gmail.com>
2024-01-23 03:47:15 +0000

    .

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: Return
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+	return (0);
+}
