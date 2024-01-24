2ecf070a73e42f1d2dceeef97e4068052030207a
“kelciatkinson” <kelciatkinson@gmail.com>
2024-01-24 21:30:55 +0000

    .

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul- here is the function
+ *
+ * @a: here is one paramater
+ *
+ * @b: here is another parameter
+ *
+ * Return: 0
+ */
+
+
+
+int mul(int a, int b)
+{
+	a * b
+	Return: (0)
+}
