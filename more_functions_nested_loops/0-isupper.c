f583c3ff94b8b0bb494df5d975018f095ce43c63
“kelciatkinson” <kelciatkinson@gmail.com>
2024-01-24 19:33:24 +0000

    .

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - check for uppercase letters
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	char letter;
+	if (letter >= 'A' && letter <= 'Z')
+		return 1;
+	else
+		return 0;
+}
