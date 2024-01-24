f88396a2fd120be17ac130fdbdf8bec1e7fb2b86
“kelciatkinson” <kelciatkinson@gmail.com>
2024-01-24 20:48:35 +0000

    .

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
 /**
- * main- here is a definition
+ * _isdigit- here is the function
+ *
+ * @c: here is the paramater
  *
  * Return: 0
  */
 
 
 
-int main()
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
