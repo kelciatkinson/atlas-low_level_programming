9b20c21f6dd06ffbbd695468e9a41defce1ca30d
“kelciatkinson” <kelciatkinson@gmail.com>
2024-01-25 17:43:10 +0000

    .

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,14 @@
 
 void print_most_numbers(void)
 {
-        char i = '0';
+char i = '0';
 
-        for (i = '0'; i <= '9'; i++)
-		if (i != '2' && i != '4')
-        	{
-                	_putchar(i);
-        	}
-		else
-			continue;
-        _putchar('\n');
+for (i = '0'; i <= '9'; i++)
+	if (i != '2' && i != '4')
+        {
+        	_putchar(i);
+        }
+	else
+		continue;
+_putchar('\n');
 }
