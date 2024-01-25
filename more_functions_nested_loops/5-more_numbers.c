9254a4ab9cff06890a9e9de2267b7f56aca50b7e
root <kelciatkinson@gmail.com>
2024-01-25 19:18:57 +0000

    .

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -16,12 +16,12 @@ void more_numbers(void)
 		{
 			if (i >= 10)
 			{
-				(i / 10) + 0;
+				(i / 10 + 0);
 				_putchar(i);
 			}
 			if (i >= 10)
 			{
-				(i % 10) + 0;
+				(i % 10 + 0);
 				_putchar(i);
 			}
 		}
