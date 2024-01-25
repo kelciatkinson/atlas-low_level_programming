e364dd2f09ce262eeee66d2dd5b8d3b01a67176e
“kelciatkinson” <kelciatkinson@gmail.com>
2024-01-25 17:21:17 +0000

    .

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * printi_most_numbers- here is the function
+ *
+ * Return: Void
+ */
+
+void print_most_numbers(void)
+{
+        char i = '0';
+
+        for (i = '0'; i <= '9'; i++)
+		if (i != 2 && i != 4)
+        	{
+                	_putchar(i);
+        	}
+        _putchar('\n');
+}
