d61bb118bfa3c8f641ac1e9b9feb798831127c1b
“kelciatkinson” <kelciatkinson@gmail.com>
2024-01-25 17:55:28 +0000

    .

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,11 @@ void more_numbers(void)
         char i = '0';
 	int count = 1;
 	for (count = 1; count < 11; count++)
+	{
 		for (i = '0'; i <= '14'; i++)
 		{
 			_putchar(i);
 		}
         	_putchar('n');
+	}
 }
