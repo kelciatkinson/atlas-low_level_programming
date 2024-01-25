769181a0418155723b9ddb316a0ab53ab1715063
“kelciatkinson” <kelciatkinson@gmail.com>
2024-01-25 16:22:20 +0000

    .

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers- here is the function 
+ *
+ * Return: Void
+ */
+
+void print_numbers(void);
+{
+	for (i = 1; i < 10; i++)
+	{
+		putchar(i);
+	}
+}
