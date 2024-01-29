37c4c4f6c606142aeaf3b2b850e905e1fa62c8a0
root <kelciatkinson@gmail.com>
2024-01-29 18:56:14 +0000

    .

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagaonals- here is a function
+ *
+ * @n: description
+ *
+ * Return: 0
+ */
+
+void print_diagonal(int n);
+{
+	int space;
+	int line;
+
+	if (n <= 0)
+	{
+		_putchar('\n)
+	}
+	else
+	{
+		for (space = 0; space < n; space++)
+	}
+}
