35fb0b59696f383d128e34a231c24f04f2952850
root <kelciatkinson@gmail.com>
2024-01-29 20:22:26 +0000

    .

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -10,5 +10,22 @@
 
 void print_square(int size)
 {
+	int i;
+	int j;
 
+	if ()
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+				_putchar('\n');
+			}
+		}
+	}
 }
