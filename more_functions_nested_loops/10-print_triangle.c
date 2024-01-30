3c3ea8bd4823b0baae60fec34723f3941c1e4f49
root <kelciatkinson@gmail.com>
2024-01-30 03:49:54 +0000

    .

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,11 +1,28 @@
 #include "main.h"
 
 /**
+ * print_triangle- here is a description
  *
+ * @size: here is a parameter
  *
- *
+ * Return: 0
  */
 
 void print_triangle(int size);
 {
+	int i;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		i = 0;
+
+		for (size = 0; i <= size; i++)
+		{
+			_putchar(35);
+		}
+	}
 }
