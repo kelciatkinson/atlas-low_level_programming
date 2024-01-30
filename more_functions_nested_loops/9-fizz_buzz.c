2e2011679d370c3faed930fbf53b687ee5815bdf
root <kelciatkinson@gmail.com>
2024-01-30 03:15:35 +0000

    .

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,11 @@ int main(void)
 	int i;
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
