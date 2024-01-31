24a59326be12b4a62ae66b61a0f642f4400b1408
“kelciatkinson” <kelciatkinson@gmail.com>
2024-01-31 16:21:32 +0000

    .

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int- here is the function
+ *
+ * @*a: pointer to an int a
+ * @*b: pointer to an int b
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap = *a;
+	*a = *b;
+	*b = swap;
+}
+
