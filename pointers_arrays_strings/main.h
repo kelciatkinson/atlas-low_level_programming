532ace57da5399c065bcfcc499d306862a7bf775
“kelciatkinson” <kelciatkinson@gmail.com>
2024-01-31 16:03:41 +0000

    .

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -4,11 +4,5 @@
 #include <stdio.h>
 
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char*s);
-void puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-
 
 #endif;
