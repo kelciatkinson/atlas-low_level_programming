f44b87a0c326277ebf0c1ffc3d991a6a3f813247
root <kelciatkinson@gmail.com>
2024-02-05 17:23:53 +0000

    .

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy- here is a description
+ *
+ * @dest: here is a parameter
+ * @src: another parameter
+ *
+ * Return: a copy of the src
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+        int index = 0;
+
+        while (*src != '\0')
+        {
+	}
+}
