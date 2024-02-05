e6d386d97fc579f3aeb827e4b58d8c525dee1534
root <kelciatkinson@gmail.com>
2024-02-05 02:35:35 +0000

    .

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,13 +14,16 @@ void puts_half(char *str)
 	while (count >= 0)
 	{
 		if (str[count] == '\0')
-		{
 			break;
-		}
 		count++;
-		n = (count - 1) / 2;
 	}
-	for ( ; 
-	printf("%d", n);
-	printf("\n");
+
+	if (count % 2 == 1)
+		n = count / 2;
+	else
+		n = (count - 1) / 2;
+
+	for (n++; n < count; n++)
+		_putchar(str[n]);
+	_putchar('\n');
 }
