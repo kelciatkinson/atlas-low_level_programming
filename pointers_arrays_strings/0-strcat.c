e4574e24dca14533e8f92f8d1bead18b2a1bb56c
root <kelciatkinson@gmail.com>
2024-02-05 23:09:39 +0000

    .

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/**
+ *
+ * _strcat- here is a description
+ *
+ * @dest: here is the first parameter
+ * @src: here is the second parameter
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return ();
+}
