18f240240857d28a154c07c99e44bcdc598ca22d
root <kelciatkinson@gmail.com>
2024-02-05 21:44:38 +0000

    .

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,29 @@
 #include "main.h"
 
-/**
+/** 
  * _atoi- here is a description
  *
- * @s: here is a paramater
+ * @s: here is a parameter
  *
- * Return: 0
+ * Return: n
  */
-
 int _atoi(char *s)
 {
-	return (0);
+	int i;
+	int n = 0;
+	int x = -1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			x *= -1;
+		}
+	}
+
+	if (x < 0)
+	{
+		n *= -1;
+	}
+	return (n);
 }
