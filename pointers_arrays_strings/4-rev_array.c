886ed292b4b4da9b3c94c0f7718ac6640db30315
root <kelciatkinson@gmail.com>
2024-02-06 03:50:23 +0000

    .

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array- here's a description
+ *
+ * @a: here's a parameter
+ * @n: here's another
+ *
+ * Return: void
+ */
+
+void reverse_array( int *a, int n)
+{
+	int i;
+	int x = 0;
+
+	n = n - 1;
+	
+	while (x <= n)
+	{
+		i = a[x];
+		a[x++] = a[n];
+		a[n--] = i;
+	}
+}
