38cee2c8da6e105b8707b08725329751f58f09b3
root <kelciatkinson@gmail.com>
2024-02-06 03:12:14 +0000

    .

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncpy- here is a description
+ *
+ * @dest:
+ * @src:
+ * @n:
+ *
+ * Return: char
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
