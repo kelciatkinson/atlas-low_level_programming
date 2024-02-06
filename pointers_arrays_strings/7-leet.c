ee74e9e152a7b85798955ca739dafd763fde3834
root <kelciatkinson@gmail.com>
2024-02-06 05:27:53 +0000

    .

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet- here's a description
+ *
+ * @s: here's a parameter
+ *
+ * Return:
+ */
+
+char *leet(char s)
+{
+	char *c = s;
+	char n[] = { 'a', 'e', 'o', 't', 'l' };
+	char x[] = { 4, 3, 0, 7, 1 };
+	int i = 0;
+
+	while (*s)
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*s == a[i] || *s == n[i] - 32)
+			{
+				*s = x[i] + '0';
+			}
+		}
+		s++;
+	}
+	return (x);
+}
