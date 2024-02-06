59a8c99126f35f013b028315d6db424369143667
root <kelciatkinson@gmail.com>
2024-02-06 00:23:51 +0000

    .

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *_strncat- here is a description
+ *
+ * @dest: char
+ * @src: char
+ * @n: int
+ *
+ * Return: char
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	return ();
+}
