11af30dce3dcc88f2a137bb9c88a4245c5e365af
root <kelciatkinson@gmail.com>
2024-02-12 19:47:18 +0000

    .

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _pow_recursion- here is a description
+ *
+ * @x: here is a parameter
+ * @y: here is another paramater
+ *
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		x = x^y;
+	}
+	return (x);
+}
