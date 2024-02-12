59a6acffc2c435db6e5cc3551a514005b2ec5cb5
root <kelciatkinson@gmail.com>
2024-02-12 21:44:30 +0000

    .

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * is_prime_number- here is a function
+ *
+ * @n: here is a parameter
+ *
+ * Return: 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n % 2 = 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
