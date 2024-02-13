ff5d499eeb8598c525ccefbb8f9f1108db5af4dd
root <kelciatkinson@gmail.com>
2024-02-13 00:27:34 +0000

    .

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main- here is a function
+ *
+ * @argc: here is a parameter
+ * @argv: here is another parameter
+ *
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
