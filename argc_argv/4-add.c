14000539ba19917ffb6f967cbde68f73329906e9
root <kelciatkinson@gmail.com>
2024-02-13 01:05:45 +0000

    .

---
+++ argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * main- here is a description
+ *
+ * @argc: here is an argument
+ * @argv: here is another argument
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv)
+{
+	int count = 0;
+
+	if (argc < 2)
+	{
+		printf("%d\n", "0");
+	}
+	else
+	{
+		while (count < argc)
+		{
+			count++;
+		}
+	}
+	printf("%d", );
+	return(0);
+}
