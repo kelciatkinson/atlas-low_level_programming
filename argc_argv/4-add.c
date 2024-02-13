071d8ebcc3644e84b8f5b0c89ae51b0e52c9d6fa
root <kelciatkinson@gmail.com>
2024-02-13 01:16:05 +0000

    .

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 			printf("Error\n");
 			return (1);
 		}
-		sum = sum + atoi(argv[count]);
+		sum = sum + *atoi(argv[count]);
 	}
 	printf("%d", sum);
 	return(0);
