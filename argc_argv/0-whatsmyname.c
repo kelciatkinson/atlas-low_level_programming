fe7726dfc046884cce56f73d8ff24f523e40bcb0
root <kelciatkinson@gmail.com>
2024-02-13 00:08:49 +0000

    .

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -4,14 +4,16 @@
  * main- here is a description
  *
  * @argc: here is a parameter
+ * @argv: here's another parameter
  *
- * Return: void
+ * Return: 
  */
 
-void main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	if (argc > 0)
 	{
 		printf("%s", argv[0]);
 	}
+	return (0);
 }
