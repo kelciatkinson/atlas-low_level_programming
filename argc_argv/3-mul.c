b90f95ca939482e9ec46478166f8d20e7e658226
root <kelciatkinson@gmail.com>
2024-02-13 00:43:06 +0000

    .

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main- here is a description
+ *
+ * @argc: here is an argument
+ * @argv: here is another argument
+ *
+ * Return: 0
+ */
+
+int main(int  argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	printf("\n", mul)
+	return (0);
+}
