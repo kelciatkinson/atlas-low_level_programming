62be4c19d15918570d0d484a98e75071c87f3ba1
root <kelciatkinson@gmail.com>
2024-02-14 17:50:15 +0000

    .

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,13 +14,9 @@ char *create_array(unsigned int size, char c)
 	char *ar;
 	int i;
 
-	while (i < size)
-		i++;
-
-	ar[i];
-
 	if (size == '\0')
 		return (0);
+	for (i = 0; i < size; i++)
 	ar = malloc(size * sizeof(c));
 
 	return(ar);
