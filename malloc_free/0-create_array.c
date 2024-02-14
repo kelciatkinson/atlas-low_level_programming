9ce154d58a3b57b132deb5c6ee3c0ae754f287fc
root <kelciatkinson@gmail.com>
2024-02-14 17:09:24 +0000

    .

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * create_array- here is a description
+ *
+ * @size: here is a parameter
+ * @c: here is another parameter
+ *
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	if (size == 0)
+		return (NULL);
+	ar = malloc(size * sizeof(c));
+	return(*ar);
+	free(ar);
+}
---
+++ malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+int _putchar();
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_gri(int **grid, int height);
+
+#endif
