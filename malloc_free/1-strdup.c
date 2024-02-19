6647a220d9a1298e63804197a94589ffee575709
root <kelciatkinson@gmail.com>
2024-02-19 17:05:27 +0000

    .

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -16,7 +16,8 @@ char *_strdup(char *str)
 
 	if (str == 0)
 		return (0);
-	for (lenstr = 0; lenstr < str; lenstr++)
+	while (str[lenstr] != '\0')
+		lenstr++;
 	
 	dup = malloc((sizeof(char) * lenstr) + 1);
 	while ( i < lenstr)
