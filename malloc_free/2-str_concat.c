495742a0169408bda9332fad81b8997ff5cf8ca3
root <kelciatkinson@gmail.com>
2024-02-19 18:48:30 +0000

    .

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _str_concat- here is a description
+ *
+ * @s1: here is a parameter
+ * @s2: here is another parameter
+ *
+ * Return: char
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	if (== 0)
+	{
+		return (0);
+	}
+
+}
