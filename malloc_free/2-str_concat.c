b1251b05d491fcf0b62811a55dfe94236ea2cf1b
root <kelciatkinson@gmail.com>
2024-02-20 01:10:00 +0000

    .

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,17 +14,19 @@ char *str_concat(char *s1, char *s2)
 {
 	char *newString;
 	int i = 0;
+	int counter;;
+	int counter2;
 	int j = 0;
 	int lenStr1, lenStr2, len;
 
 	if (s1 != NULL)
 	{
-		for (i = 0; s1[i] != '\0'; i++)
+		for (counter = 0; s1[counter] != '\0'; counter++)
 			lenStr1++;
 	}
 	if (s2 != NULL)
 	{
-		for (i = 0; s2[i] != '\0'; i++)
+		for (counter2 = 0; s2[counter2] != '\0'; counter2++)
 			lenStr2++;
 	}
 	len = lenStr1 + lenStr1;
