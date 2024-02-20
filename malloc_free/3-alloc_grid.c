09bac76b28aa11c1e28d4e7beedfe530db824a57
root <kelciatkinson@gmail.com>
2024-02-20 02:15:45 +0000

    .

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -16,12 +16,9 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-
 	arr = (int **)malloc(sizeof(int *) * height);
-	
 	if (arr == NULL)
 		return (NULL);
-	
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *)malloc(sizeof(int) * width);
@@ -39,5 +36,5 @@ int **alloc_grid(int width, int height)
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 	}
-	return (arr);
+return (arr);
 }
