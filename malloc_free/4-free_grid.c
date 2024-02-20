7926c6c92a5cfe8b7e72c0b3e71e97801c36b8b8
root <kelciatkinson@gmail.com>
2024-02-20 02:21:24 +0000

    .

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid- here is a description
+ *
+ * @grid: here is a parameter
+ * @height: here is another parameter
+ *
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i; 
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
