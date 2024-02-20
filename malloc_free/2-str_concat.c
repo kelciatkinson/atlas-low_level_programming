5004cb1fd3e68a497a0d38fa04a73c7a19ebc40d
root <kelciatkinson@gmail.com>
2024-02-20 00:45:27 +0000

    .

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -31,7 +31,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	newString = (char *)malloc((sizeof(char) * (lenStr1 + lenStr2 + 1)));
+	newString = (char *)malloc((sizeof(char) * (lenStr1 + lenStr2) + 1));
 	if (newString == NULL)
 		return (NULL);
 	while (s1[i] != '\0')
