12fddc116ac8ffbce60bd4e5b6516ebad74ca8da
Kelci Atkinson <kelciatkinson@gmail.com>
2024-02-21 13:11:13 -0600

    .

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
@@ -9,7 +10,11 @@ struct dog {
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-/*dog_t *new_dog(char *name, float age, char *owner);*/
-/*void free_dog(dog_t *d);*/
+/**
+* dog_t *new_dog(char *name, float age, char *owner); 
+*/
+/**
+* void free_dog(dog_t *d); 
+*/
 
 #endif
