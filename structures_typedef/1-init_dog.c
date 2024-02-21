2dec03df65628b44a79f529ea1a1353bec91921c
Kelci Atkinson <kelciatkinson@gmail.com>
2024-02-21 13:36:41 -0600

    .

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog- here is a description
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog
+	{
+	}
+}
