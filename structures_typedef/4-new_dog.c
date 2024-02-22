088806dbe039ead89028c98c5e80548b40fd5fa6
root <kelciatkinson@gmail.com>
2024-02-22 22:07:46 +0000

    .

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,7 +13,11 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct Doggy *new_dog;
-
+	struct Doggy *new_dog
+	{
+		new_dog->name = name;
+		new_dog->age = age;
+		new_dog->owner = owner;
+	};
 	return(Doggy);
 }
