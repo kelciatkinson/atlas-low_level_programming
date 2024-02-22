b1c532b93867af24b3b7cf299c5c916ffa0da0e2
root <kelciatkinson@gmail.com>
2024-02-22 22:02:57 +0000

    .

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog- here is a description
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: new_dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct Doggy *new_dog;
+
+	return(Doggy);
+}
