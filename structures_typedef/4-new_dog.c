6f6fe5bccd44d330162bcc1e4787634372625d8d
root <kelciatkinson@gmail.com>
2024-02-27 04:01:56 +0000

    .

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -14,17 +14,26 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i;
+	int lenName;
 	dog_t *Doggy = malloc(sizeof(dog_t));
 	if (Doggy == NULL)
 		return (NULL);
-	Doggy->name = malloc(sizeof(name) + 1);
-	Doggy->owner = malloc(sizeof(owner) + 1);
+	lenName= sizeof(name) + 1;
+	Doggy->name = malloc(lenName);
 
 	if (Doggy->name == NULL)
 	{
 		return (NULL);
 		free(Doggy);
 	}
+
+	for (i = 0; i < lenName; i++)
+	{
+		Doggy->name[i] = name[i];
+	}
+	Doggy->owner = malloc(sizeof(owner) + 1);
+
 	if (Doggy->age == 0)
 		return (0);
 	if (Doggy->owner == NULL)
@@ -32,7 +41,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 		free(Doggy->name);
 	}
-	Doggy->name = name;
 	Doggy->age = age;
 	Doggy->owner = owner;
 	return (Doggy);
