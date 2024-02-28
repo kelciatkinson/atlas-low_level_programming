59e6df99d576a1898d18dc7498cf6254845f1edb
root <kelciatkinson@gmail.com>
2024-02-28 01:12:39 +0000

    .

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,73 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add- here is a description
+ *
+ * @a: here is a parameter
+ * @b: here is another parameter
+ *
+ * Return: a + b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub- here is a description
+ *
+ * @a: here is a parameter
+ * @b: here is another parameter
+ * 
+ * Return: a - b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul- here is a description
+ *
+ * @a: here is a parameter
+ * @b: here is another parameter
+ *
+ * Return: a * b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div- here is a description
+ *
+ * @a: here is a parameter
+ * @b: here is another parameter
+ *
+ * Return: a / b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod- here is a description
+ *
+ * @a: here is a parameter
+ * @b: here is another parameter
+ *
+ * Return: remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
