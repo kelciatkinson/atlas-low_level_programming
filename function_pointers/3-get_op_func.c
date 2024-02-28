642a951fe8b624cc74c022cee3d5801b99efe943
root <kelciatkinson@gmail.com>
2024-02-28 01:22:34 +0000

    .

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <string.h>
+
+/**
+ * get_op_func- here is a description
+ *
+ * @s: here is a parameter
+ *
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			break;
+		i++;
+	}
+	return (ops[i].f);
+}
