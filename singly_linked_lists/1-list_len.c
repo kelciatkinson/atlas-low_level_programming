30da1b024ad7c3f2b48cf83762be81477b460f4f
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-06 22:37:15 +0000

    testing without current

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -11,12 +11,11 @@
 size_t list_len(const list_t *h)
 {
 	size_t count = 0;
-	const list_t *current = h;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
 		count++;
-		current = current->next;
+		h = h->next;
 	}
 	return (count);
 }
