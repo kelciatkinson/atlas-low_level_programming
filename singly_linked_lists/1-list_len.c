b3e6b1506952b6d40681ea58926a44668f25e2e6
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-06 21:59:39 +0000

    set up file 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * list_len- here is a description
+ *
+ * @h: here is a parameter
+ *
+ * Return: # of elements in linked list_t list
+ */
+
+size_t list_len(const list_t *h)
+{
+	Return (0);
+}
