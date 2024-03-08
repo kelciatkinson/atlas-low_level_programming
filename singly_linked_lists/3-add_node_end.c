828033f2a169c841375359eddd80eaddd182d521
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-08 23:21:13 +0000

    testing head end

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end- here is a description
+ *
+ * @head: here is a parameter
+ * @str: here is another parameter
+ *
+ * Return: address
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	char *temp;
+	list_t *new;
+	int len;
+
+	new = malloc(sizeof(list_t));
+
+	temp = strdup(str);
+	if (new == NULL)
+		return (NULL);
+
+	if (temp == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (len = 0; str[len];)
+		len++;
+	new->str = temp;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
