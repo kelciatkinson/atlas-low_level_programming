075f70f0ef046adcb04e40a315b61570f8765272
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-08 23:17:23 +0000

    spacing

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -27,15 +27,11 @@ list_t *add_node(list_t **head, const char *str)
 		free(new);
 		return (NULL);
 	}
-
 	for (len = 0; str[len];)
-		len++;
-	
+		len++;	
 	new->str = temp;
 	new->len = len;
 	new->next = *head;
-
 	*head = new;
-	
 	return (*head);
 }
