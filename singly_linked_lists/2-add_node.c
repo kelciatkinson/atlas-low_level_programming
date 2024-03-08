eac6a59b07d0b1220184564d536d15c2bdfa8196
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-08 20:41:06 +0000

    adding file 2

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * add_node- here is a description
+ *
+ * @head: here is a parameter
+ * @str: here is another parameter
+ *
+ * Return: address
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	return (NULL);
+	return (address of new node);
+}
