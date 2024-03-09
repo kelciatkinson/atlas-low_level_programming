162a464e341b43f1dc043718e959b281a5c67dc3
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-09 21:07:59 +0000

    testing file 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint- here is a description
+ *
+ * @h: here is a parameter
+ *
+ * Return: number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	const listint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		count++;
+		printf("%s", temp);
+		temp = temp->next;
+	}
+	return (count);
+}
---
+++ more_singly_linked_lists/lists.h
@@ -0,0 +1,11 @@
+#ifndef LISTS
+#define LISTS
+
+#include <unistd.h>
+
+int _putchar(char c)
+	return (write(1, &c, 1));
+}
+size_t print_listint(const listint_t *h);
+
+#endif
