9a842023fa0ae33271284729bf18bb84b859eb51
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-09 21:10:50 +0000

    added struct

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -3,6 +3,20 @@
 
 #include <unistd.h>
 
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
