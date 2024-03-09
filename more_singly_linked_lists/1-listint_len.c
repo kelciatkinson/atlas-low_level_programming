70d00cad5c02753af9c7bfeae131700a823fc29b
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-09 22:30:56 +0000

    testing

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -11,10 +11,12 @@
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
+	const listint_t *temp = h;
 
-	while (listint_t != NULL)
+	while (temp != NULL)
 	{
 		count++;
+		temp = temp->next;
 	}
 	return (count);
 }
