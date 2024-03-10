37b80975b86ea8db067702c5945fd4600fc78bad
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-10 00:15:59 +0000

    testing file 2

---
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_nodeint- here is a description
+ *
+ * @head: here is a parameter
+ * @n: here is another parameter
+ *
+ * Return: 
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	char *temp;
+	listint_t *new;
+	int len;
+
+	new = malloc(sizeof(list_t));
+
+	temp = strdup(str);
+
+	if (new == NULL)
+		return (NULL);
+
+	if (temp == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len])
+		len++;
+
+	new->str = temp;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
