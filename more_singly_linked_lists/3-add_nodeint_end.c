8f6b56823f829fbe1209d24f2cf4cdc4ea1d5e6c
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-11 21:18:29 +0000

    set up file 3 and added prototype to header file

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end- here is a description
+ *
+ * @head: here is a parameter
+ * @n: here is another parameter
+ *
+ * Return: 
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	return ();
+}
