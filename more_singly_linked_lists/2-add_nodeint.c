f39dea1680a26e7f172572e19aed3376e7466cf1
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-11 20:59:49 +0000

    .

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -11,3 +11,16 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
+	listint_t *new;
+	
+	new = malloc(sizeof(listin_t));
+
+	while (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
