cc3029d57919e6c00688b8a101889c21f98ebd7e
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-11 21:03:07 +0000

    trailing whitespace

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -6,15 +6,13 @@
  * @head: here is a parameter
  * @n: here is another parameter
  *
- * Return: 
+ * Return: new
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
-	
 	new = malloc(sizeof(listint_t));
-
 	while (new == NULL)
 		return (NULL);
 
