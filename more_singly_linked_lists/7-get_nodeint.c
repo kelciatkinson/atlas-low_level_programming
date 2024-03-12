099a5133f1ef635f7cbce880e28aa90a55e08f9c
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-12 02:01:50 +0000

    setup file 7

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index- here is a description
+ *
+ * @head: here is a parameter
+ * @index: here is another parameter
+ *
+ * Return: listint_t
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+
+	return (listint_t);
+}
