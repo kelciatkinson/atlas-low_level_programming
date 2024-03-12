f16a93a613dd93781b7714629c2576fbcb851891
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-12 03:15:23 +0000

    working

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -11,6 +11,15 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	unsigned int i;
 
-	return (listint_t);
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
+	}
+	return (head);
 }
