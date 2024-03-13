5598809265d5a219ed2c5e779ff9be9e558d84dc
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-13 02:07:03 +0000

    working

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * binary_to_uint- here is a description
@@ -10,5 +11,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-
+	unsigned int i;
+	unsigned int n = 0;
+	
+	if (b == NULL)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		n = n * 2 + b[i] - '0';
+	}
+	return (n);
 }
