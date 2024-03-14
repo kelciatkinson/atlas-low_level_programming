9c0e21fb9d57f03e319437cca93592ab5ff2dd05
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-14 18:29:38 +0000

    trying != NULL

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -10,4 +10,18 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
+	int count = 0;
+
+	const dlistint_t *new = h;
+
+	while (new != NULL)
+	{
+		if (new->n != NULL)
+		{
+			printf("[%d] %s\n", new->n);
+			new = new->next;
+			count++;
+		}
+	}
+	return (count);
 }
