648d5c229206f3fa3fef7057ea1ef961b1c03171
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-14 18:42:26 +0000

    testing temp

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -11,11 +11,12 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
+	const dlistint_t *temp = h;
 	
-	while (h != NULL)
+	while (temp != NULL)
 	{
 		count++;
-		h = h->next;
+		temp = temp->next;
 	}
 	return (count);
 }
