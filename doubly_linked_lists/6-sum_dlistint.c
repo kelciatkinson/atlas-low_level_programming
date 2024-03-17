1e79b111eb8b9d4978a27559d89eacc479bdc658
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-17 20:53:40 +0000

    testing file 6

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint- here is a description
+ *
+ * @head: here is a parameter
+ *
+ * Return: 0
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (head != NULL)
+	{
+		sum += head->n;
+	}
+	return (sum);
+}
