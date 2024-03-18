5346df2b5246aaa24d2500bb60c5294c22eb916c
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-18 06:36:22 +0000

    setting up files in has_tables

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create- here is a description
+ *
+ * @size: here is a parameter
+ *
+ * Return: pointer
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	return (new);
+}
