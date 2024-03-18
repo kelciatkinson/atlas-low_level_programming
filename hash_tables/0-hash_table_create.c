8e917db2184803175dd6bd1d964bec3f1c940b4c
“kelciatkinson” <kelciatkinson@gmail.com>
2024-03-18 16:53:11 +0000

    betty

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -19,10 +19,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	new->size = size;
 	new->array = malloc(sizeof(hash_node_t *) * size);
-	
+
 	if (new->array == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < size; i++)
 		new->array[i] = NULL;
 	return (new);
