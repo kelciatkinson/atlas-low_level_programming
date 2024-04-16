60c9febc019a321dec06517843edcc3c85ee6eb6
“kelciatkinson” <kelciatkinson@gmail.com>
2024-04-16 04:07:46 +0000

    fixed trailing whitespace

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -17,7 +17,7 @@ int binary_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-	
+
 	for (left = 0, right = size - 1; left <= right;)
 	{
 		printf("Searching in array: ");
